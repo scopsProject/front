31bf4db0a38bb2d762ce7ce23a096f07529e2f85
yoobin02 <yoobin0821@naver.com>
2025-11-12 13:05:26 +0900

    251112

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -19,7 +19,7 @@ function LoginPage() {
   }
   else
   {
-    axios.post(`${process.env.REACT_APP_API_URL}/api/scops/login`, {
+    axios.post(`${process.env.REACT_APP_API_URL}/scops/login`, {
     userID: studentId,
     password: password,
   })
